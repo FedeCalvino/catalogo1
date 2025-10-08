7859e021aca43cd130b0b89925b120ad8097cc30
Fitzroyfang <Calvinofede@gmail.com>
2025-10-08 13:41:22 -0300

    arr2

--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,3 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js')
-      .then(reg => console.log('Service Worker registrado', reg))
-      .catch(err => console.log('Error al registrar el Service Worker', err));
-  });
-}
